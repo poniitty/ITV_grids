806f06fcbea77108e5d6c7515657006203766487
jkemppinen <julia.kemppinen@gmail.com>
2022-04-06 14:36:30 +0300

    new plots: itv per grid & within grid trait variation per species

--- scripts/summary_plots.R
+++ scripts/summary_plots.R
@@ -33,7 +33,7 @@ trait_names <- c(
   
 )
 
-# plot
+# plot all grids together
 p1 = td %>% select(id, abbr, height:LDMC) %>% 
   pivot_longer(cols = height:LDMC, names_to = "trait") %>% 
   ggplot(aes(y = value, x = abbr, fill = abbr, colour = abbr)) +
@@ -59,5 +59,94 @@ dev.off()
 pdf(file="visuals/ITV_summary.pdf", width = 7.48, height = 9.45)
 
 p1 & theme(plot.tag = element_text(size = 8))
+
+dev.off()
+
+# rename and reorder species: first the forbs, then the shrubs
+td$grid<- recode_factor(td$grid,
+                        A = "Forest, poor",
+                        B = "Forest, rich",
+                        C = "Ecotone, poor",
+                        D = "Ecotone, rich",
+                        E = "Tundra, poor",
+                        F = "Tundra, rich")
+
+# plot
+p2 = td %>% select(id, grid, abbr, height:LDMC) %>% 
+  pivot_longer(cols = height:LDMC, names_to = "trait") %>% 
+  ggplot(aes(y = value, x = grid, fill = grid, colour = grid)) +
+  geom_boxplot (notch=TRUE, notchwidth = 0.1, width=0.8, outlier.shape = NA, lwd=0.5, alpha=7/10) +
+  geom_jitter(alpha = 0.3, width = 0.2, size = 2) +
+  facet_wrap(vars(trait), scales = "free_y", labeller = as_labeller(trait_names)) +
+  scale_fill_met_d("Peru1") +
+  scale_colour_met_d("Peru1") +
+  ylab("Trait value") +
+  xlab(NULL) +
+  labs(fill = "Grid", colour = "Grid") +
+  theme_classic() +
+  theme(
+    aspect.ratio = 1,
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    strip.background = element_blank(),
+    legend.position="bottom")
+
+# save figure
+dev.off()
+pdf(file="visuals/ITV_summary_bygrid.pdf", width = 7.48, height = 9.45)
+
+p2 & theme(plot.tag = element_text(size = 8))
+
+dev.off()
+
+# plot
+p3 = td %>% select(id, grid, abbr, height:LDMC) %>% 
+  pivot_longer(cols = height:LDMC, names_to = "trait") %>% 
+  ggplot(aes(y = value, x = grid, fill = abbr, colour = abbr)) +
+  geom_boxplot (notch=TRUE, notchwidth = 0.1, width=0.8, outlier.shape = NA, lwd=0.5, alpha=7/10) +
+  facet_wrap(vars(trait), nrow=4, scales = "free_y", labeller = as_labeller(trait_names)) +
+  scale_fill_met_d("Hokusai3") +
+  scale_colour_met_d("Hokusai3") +
+  ylab("Trait value") +
+  xlab(NULL) +
+  labs(fill = "Species", colour = "Species") +
+  theme_classic() +
+  theme(
+    axis.ticks.x = element_blank(),
+    strip.background = element_blank(),
+    legend.position="bottom",
+    legend.text = element_text(face = "italic"))
+
+# save figure
+dev.off()
+pdf(file="visuals/ITV_summary_bygrid_byspecies.pdf", width = 7.48, height = 9.45)
+
+p3 & theme(plot.tag = element_text(size = 8))
+
+dev.off()
+
+# plot
+p4 = td %>% select(id, grid, abbr, height:LDMC) %>% 
+  pivot_longer(cols = height:LDMC, names_to = "trait") %>% 
+  ggplot(aes(y = value, x = abbr, fill = grid, colour = grid)) +
+  geom_boxplot (notch=TRUE, notchwidth = 0.1, width=0.8, outlier.shape = NA, lwd=0.5, alpha=7/10) +
+  facet_wrap(vars(trait), nrow=4, scales = "free_y", labeller = as_labeller(trait_names)) +
+  scale_fill_met_d("Peru1") +
+  scale_colour_met_d("Peru1") +
+  ylab("Trait value") +
+  xlab(NULL) +
+  labs(fill = "Grid", colour = "Grid") +
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(face = "italic"),
+    axis.ticks.x = element_blank(),
+    strip.background = element_blank(),
+    legend.position="bottom")
+
+# save figure
+dev.off()
+pdf(file="visuals/ITV_summary_byspecies_bygrid.pdf", width = 7.48, height = 9.45)
+
+p4 & theme(plot.tag = element_text(size = 8))
 
 dev.off()
